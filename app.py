e3ff46cfcd8ab6a2f87c7efdfc04caddf6dec8ca
Aditya Ramesh <adityaramesh15@gmail.com>
2024-05-14 11:10:54 -0500

    Completed Sprint 1

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 todo_list = []
 
+try:
+    with open("todo_list.txt", "r") as file:
+        for line in file:
+            todo_list.append(line.strip())
+except FileNotFoundError:
+    print("No saved items to be found")
+
+
+
 while True:
     print("----------------------------")
     print("To Do List")
@@ -29,6 +38,11 @@ while True:
         continue
 
     if choice == "X":
+        with open("todo_list.txt", "w") as file:
+            for todo in todo_list:
+                file.write(f"{todo}\n")
         break
 
     print("\nInvalid Choice")
+
+    
