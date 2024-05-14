4a45af7d83e8ead2ada75956cd244afbc5f2dd84
Aditya Ramesh <adityaramesh15@gmail.com>
2024-05-14 13:19:33 -0500

    Completed Sprint 2

--- app.py
+++ app.py
@@ -1,48 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+todo_file = os.path.join(basedir, "todo_list.txt")
+
 todo_list = []
 
 try:
-    with open("todo_list.txt", "r") as file:
+    with open(todo_file, "r") as file:
         for line in file:
             todo_list.append(line.strip())
 except FileNotFoundError:
     print("No saved items to be found")
 
 
+@app.route("/")
+def index():
+    return render_template("index.html", todo_list=todo_list)
 
-while True:
-    print("----------------------------")
-    print("To Do List")
-    num = 1
-    for todo in todo_list:
-        print(f"{num}: {todo}")
-        num += 1
-    
-    
-    print()
-    print("Actions: ")
-    print("A - Add to-do item")
-    print("R - Remove to-do item")
-    print("X - Exit")
-    choice = input("Enter your choice (A, R, or X): ").upper()
+@app.route("/add", methods=["POST"])
+def add_todo():
+    todo = request.form["todo"]
+    todo_list.append(todo)
+    save_todo_list()
+    return redirect(url_for("index"))
+def save_todo_list():
+    with open(todo_file, "w") as file:
+        for todo in todo_list:
+            file.write(todo + "\n")
 
-    if choice == "A":
-        todo = input("Enter the to-do item: ")
-        todo_list.append(todo)
-        continue
-    elif choice == "R":
-        num = int(input("Enter the number of the item to remove: "))
-        if num > 0 and num <= len(todo_list):
-            todo_list.pop(num - 1)
-        else: 
-            print("Invalid Number")
-        continue
+@app.route("/remove", methods=["POST"])
+def remove_todo():
+    item_number = int(request.form["item_number"])
+    if 0 < item_number and item_number <= len(todo_list):
+        todo_list.pop(item_number-1)
+        save_todo_list()
+    return redirect(url_for("index"))
 
-    if choice == "X":
-        with open("todo_list.txt", "w") as file:
-            for todo in todo_list:
-                file.write(f"{todo}\n")
-        break
 
-    print("\nInvalid Choice")
-
-    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ consoletest.py
@@ -0,0 +1,46 @@
+todo_list = []
+
+try:
+    with open(todo_file, "r") as file:
+        for line in file:
+            todo_list.append(line.strip())
+except FileNotFoundError:
+    print("No saved items to be found")
+
+
+
+while True:
+    print("----------------------------")
+    print("To Do List")
+    num = 1
+    for todo in todo_list:
+        print(f"{num}: {todo}")
+        num += 1
+    
+    
+    print()
+    print("Actions: ")
+    print("A - Add to-do item")
+    print("R - Remove to-do item")
+    print("X - Exit")
+    choice = input("Enter your choice (A, R, or X): ").upper()
+
+    if choice == "A":
+        todo = input("Enter the to-do item: ")
+        todo_list.append(todo)
+        continue
+    elif choice == "R":
+        num = int(input("Enter the number of the item to remove: "))
+        if num > 0 and num <= len(todo_list):
+            todo_list.pop(num - 1)
+        else: 
+            print("Invalid Number")
+        continue
+
+    if choice == "X":
+        with open("todo_list.txt", "w") as file:
+            for todo in todo_list:
+                file.write(f"{todo}\n")
+        break
+
+    print("\nInvalid Choice")
