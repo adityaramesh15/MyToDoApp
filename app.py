9dd7252f85a2a4fb0a95155815ad4f1e1466f0e3
Aditya Ramesh <adityaramesh15@gmail.com>
2024-05-15 17:26:36 -0500

    Completed Sprint 5

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, g
 from database import db, Todo
+from recommendation_engine import RecommendationEngine
 import os
+import json
 
 app = Flask(__name__)
 
@@ -38,6 +40,42 @@ def remove_todo(id):
     db.session.delete(Todo.query.filter_by(id=id).first())
     db.session.commit()
     return redirect(url_for("index"))
+
+
+@app.route("/recommend/<int:id>", methods=["GET"])
+@app.route("/recommend/<int:id>/<refresh>", methods=["GET"])
+async def recommend(id, refresh = False):
+    recommendation_engine = RecommendationEngine()
+    g.todo = db.session.query(Todo).filter_by(id = id).first()
+
+    if g.todo and not refresh:
+        try:
+            if g.todo.recommendations_json is not None:
+                g.todo.recommendations = json.loads(g.todo.recommendations_json)
+                return render_template("index.html")
+        except ValueError as e:
+            print("Error: ", e)
+
+    previous_links_str = None
+    if refresh:
+        g.todo.recommendations = json.loads(g.todo.recommendations_json)
+        links = [item["link"] for item in g.todo.recommendations]
+        previous_links_str = ", ".join(links)
+
+
+    g.todo.recommendations = await recommendation_engine.get_recommendations(g.todo.name, previous_links_str)
+
+    try:
+        g.todo.recommendations_json = json.dumps(g.todo.recommendations)
+        db.session.add(g.todo)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error adding and committing todo: {e}")
+        return  
+
+
+    return render_template("index.html")
+
 
 
 if __name__ == "__main__":
